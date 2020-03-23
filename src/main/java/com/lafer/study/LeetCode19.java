2d2ad70534e6f4d7d4fdcee172212196ef23be17
liangfeng <1621159074@qq.com>
2020-03-23 22:20:18 +0800

    [修改说明]LeetCode19、876

---
+++ src/main/java/com/lafer/study/LeetCode19.java
@@ -0,0 +1,44 @@
+package com.lafer.study;
+
+import com.sun.xml.internal.org.jvnet.fastinfoset.FastInfosetException;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 思考：快慢指针，快指针比满指针先走n步
+ *
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ */
+
+public class LeetCode19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        while(n-- != 0) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
+
+
+
