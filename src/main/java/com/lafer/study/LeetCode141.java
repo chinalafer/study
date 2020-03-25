74f73393a2180eb01c17b114677f85b9efdaec19
liangfeng <1621159074@qq.com>
2020-03-25 14:26:11 +0800

    [修改说明]LeetCode141

---
+++ src/main/java/com/lafer/study/LeetCode141.java
@@ -0,0 +1,20 @@
+package com.lafer.study;
+
+/**
+ * 给定一个链表，判断链表中是否有环。
+ */
+public class LeetCode141 {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
